f1c23808a7655ddba4fac72db61f845016707e00
202010094 SETHOSA <202010094@SWAVE.smu.ac.za>
2024-05-08 16:06:26 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,26 @@
-import { defineConfig } from 'vite'
+/*import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/diabetes/',
+  //base: '/diabetes/',
 })
+
+*/
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    // include: ['linked-dep'],
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+      // include: [/linked-dep/, /node_modules/],
+    },
+  },
+});
